bd4ce2def8b5189433971b5d2abdf0836549dc25
Srinivasa Arun Yeragudipati <38272686+arun-y99@users.noreply.github.com>
2019-12-20 10:18:37 +0530

    exercise1

---
+++ MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button b;
+    EditText n1,n2,n3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b = findViewById(R.id.button);
+        n1 = findViewById(R.id.n1);
+        n2 = findViewById(R.id.n2);
+        n3 = findViewById(R.id.n3);
+
+        b.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                double x = Double.parseDouble(n1.getText().toString()) + Double.parseDouble(n2.getText().toString());
+                n3.setText(Double.toString(x));
+            }
+        });
+    }
+
+
+}
