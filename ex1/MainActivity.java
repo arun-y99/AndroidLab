4a91256f70e141fca1fc56e6036e70966b10bdb1
Srinivasa Arun Yeragudipati <38272686+arun-y99@users.noreply.github.com>
2020-01-03 12:05:22 +0530

    Update MainActivity.java

--- ex1/MainActivity.java
+++ ex1/MainActivity.java
@@ -2,34 +2,79 @@ package com.example.ex1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.RadioButton;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Calendar;
 
-    Button b;
-    EditText n1,n2,n3;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button b,t;
+    EditText n1,n2,n3,n4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         b = findViewById(R.id.button);
+        t = findViewById(R.id.time);
         n1 = findViewById(R.id.n1);
         n2 = findViewById(R.id.n2);
         n3 = findViewById(R.id.n3);
+        n4 = findViewById(R.id.n4);
 
-        b.setOnClickListener(new View.OnClickListener() {
+        b.setOnClickListener(this);
+    }
+        @Override
+        public void onClick(View v) {
+            if (v == b) {
 
-            @Override
-            public void onClick(View v) {
-                double x = Double.parseDouble(n1.getText().toString()) + Double.parseDouble(n2.getText().toString());
-                n3.setText(Double.toString(x));
+                // Get Current Date
+                final Calendar c = Calendar.getInstance();
+                int mYear = c.get(Calendar.YEAR);
+                int mMonth = c.get(Calendar.MONTH);
+                int mDay = c.get(Calendar.DAY_OF_MONTH);
+
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+
+                                n4.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                            }
+                        }, mYear, mMonth, mDay);
+                datePickerDialog.show();
             }
-        });
+        }
+
+
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radio_pirates:
+                if (checked)
+                    // Pirates are the best
+                    break;
+            case R.id.radio_ninjas:
+                if (checked)
+                    // Ninjas rule
+                    break;
+        }
+    }
     }
 
 
-}
+
