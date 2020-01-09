5b558408cb31f0680bb357e94716edac3840d517
Srinivasa Arun Yeragudipati <ysarun1999@gmail.com>
2020-01-09 20:11:58 +0530

    do ex1 and ex2

--- ex1/MainActivity.java
+++ ex1/MainActivity.java
@@ -1,21 +1,26 @@
-package com.example.ex1;
+package com.example.androidlab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.TimePicker;
 
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     Button b,t;
-    EditText n1,n2,n3,n4;
+    EditText n1,n2,n3,n4,n6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,34 +32,64 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         n2 = findViewById(R.id.n2);
         n3 = findViewById(R.id.n3);
         n4 = findViewById(R.id.n4);
-
+        n6 = findViewById(R.id.n6);
         b.setOnClickListener(this);
+        t.setOnClickListener(this);
+
+        Spinner spinner =  findViewById(R.id.marital);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.marital, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
-        @Override
-        public void onClick(View v) {
-            if (v == b) {
+    @Override
+    public void onClick(View v) {
+        if (v == b) {
 
-                // Get Current Date
-                final Calendar c = Calendar.getInstance();
-                int mYear = c.get(Calendar.YEAR);
-                int mMonth = c.get(Calendar.MONTH);
-                int mDay = c.get(Calendar.DAY_OF_MONTH);
+            // Get Current Date
+            final Calendar c = Calendar.getInstance();
+            int mYear = c.get(Calendar.YEAR);
+            int mMonth = c.get(Calendar.MONTH);
+            int mDay = c.get(Calendar.DAY_OF_MONTH);
 
 
-                DatePickerDialog datePickerDialog = new DatePickerDialog(this,
-                        new DatePickerDialog.OnDateSetListener() {
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                    new DatePickerDialog.OnDateSetListener() {
 
-                            @Override
-                            public void onDateSet(DatePicker view, int year,
-                                                  int monthOfYear, int dayOfMonth) {
+                        @Override
+                        public void onDateSet(DatePicker view, int year,
+                                              int monthOfYear, int dayOfMonth) {
 
-                                n4.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+                            n4.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
 
-                            }
-                        }, mYear, mMonth, mDay);
-                datePickerDialog.show();
-            }
+                        }
+                    }, mYear, mMonth, mDay);
+            datePickerDialog.show();
         }
+        if (v == t) {
+
+            // Get Current Time
+            final Calendar c = Calendar.getInstance();
+            int mHour = c.get(Calendar.HOUR_OF_DAY);
+            int mMinute = c.get(Calendar.MINUTE);
+
+            // Launch Time Picker Dialog
+            TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                    new TimePickerDialog.OnTimeSetListener() {
+
+                        @Override
+                        public void onTimeSet(TimePicker view, int hourOfDay,
+                                              int minute) {
+
+                            n6.setText(hourOfDay + ":" + minute);
+                        }
+                    }, mHour, mMinute, false);
+            timePickerDialog.show();
+        }
+    }
 
 
 
@@ -64,17 +99,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         // Check which radio button was clicked
         switch(view.getId()) {
-            case R.id.radio_pirates:
+            case R.id.male:
                 if (checked)
                     // Pirates are the best
                     break;
-            case R.id.radio_ninjas:
+            case R.id.female:
                 if (checked)
                     // Ninjas rule
                     break;
         }
     }
+
+    public void onCheckboxClicked(View view) {
+        // Is the view now checked?
+        boolean checked = ((CheckBox) view).isChecked();
+
+        // Check which checkbox was clicked
+        switch(view.getId()) {
+            case R.id.alcohol:
+                if (checked)
+                    ;
+            else
+                break;
+            case R.id.smoking:
+                if (checked)
+                    ;
+            else
+                break;
+            // TODO: Veggie sandwich
+        }
     }
+
+    public void onSubmitClicked(View view) {
+
+    }
+
+    public void onResetClicked(View view) {
+
+    }
+}
 
 
 
--- ex2/MainActivity.java
+++ ex2/MainActivity.java
@@ -1,70 +1,172 @@
-package com.example.ex2;
+package com.example.androidlab;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.TimePicker;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Calendar;
 
-    Button clickBtn, longClickBtn, allBtn, btnShow;
-    TextView sample;
-    EditText nameTxt;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
+
+    Button b,t;
+    EditText n1,n2,n3,n4,n5,n6;
+    String gender,marital,addiction;
+    public static final String EXTRA_MESSAGE = "com.example.androidlab.MESSAGE";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b = findViewById(R.id.button);
+        t = findViewById(R.id.time);
+        n1 = findViewById(R.id.n1);
+        n2 = findViewById(R.id.n2);
+        n3 = findViewById(R.id.n3);
+        n4 = findViewById(R.id.n4);
+        n5 = findViewById(R.id.n5);
+        n6 = findViewById(R.id.n6);
+        b.setOnClickListener(this);
+        t.setOnClickListener(this);
 
-        clickBtn =  findViewById(R.id.btnClick);
-        longClickBtn =  findViewById(R.id.btnLongClick);
-        allBtn =  findViewById(R.id.btnAll);
-        btnShow =  findViewById(R.id.btnShowName);
-        sample =  findViewById(R.id.txtSample);
-        nameTxt =  findViewById(R.id.txtName);
 
-        /*Simple Click onClick Listener*/
-        clickBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Hey feller!", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        longClickBtn.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                Toast.makeText(getApplicationContext(), "Hey there!",
-                        Toast.LENGTH_SHORT).show();
-                return false;
-            }
-        });
-
-        allBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "You Just Clicked Me!",
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
-        allBtn.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View v) {
-                Toast.makeText(getApplicationContext(), "You clicked me for so long!",
-                        Toast.LENGTH_SHORT).show();
-                return false;
-            }
-        });
-
-        btnShow.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sample.setText(nameTxt.getText().toString());
-            }
-        });
+        Spinner spinner =  findViewById(R.id.marital);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.marital, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this)
     }
+    @Override
+    public void onClick(View v) {
+        if (v == b) {
+
+            // Get Current Date
+            final Calendar c = Calendar.getInstance();
+            int mYear = c.get(Calendar.YEAR);
+            int mMonth = c.get(Calendar.MONTH);
+            int mDay = c.get(Calendar.DAY_OF_MONTH);
+
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                    new DatePickerDialog.OnDateSetListener() {
+
+                        @Override
+                        public void onDateSet(DatePicker view, int year,
+                                              int monthOfYear, int dayOfMonth) {
+
+                            n4.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                        }
+                    }, mYear, mMonth, mDay);
+            datePickerDialog.show();
+        }
+        if (v == t) {
+
+            // Get Current Time
+            final Calendar c = Calendar.getInstance();
+            int mHour = c.get(Calendar.HOUR_OF_DAY);
+            int mMinute = c.get(Calendar.MINUTE);
+
+            // Launch Time Picker Dialog
+            TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                    new TimePickerDialog.OnTimeSetListener() {
+
+                        @Override
+                        public void onTimeSet(TimePicker view, int hourOfDay,
+                                              int minute) {
+
+                            n6.setText(hourOfDay + ":" + minute);
+                        }
+                    }, mHour, mMinute, false);
+            timePickerDialog.show();
+        }
+    }
+
+
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.male:
+                if (checked)
+                    gender = "male";
+                    break;
+            case R.id.female:
+                if (checked)
+                    gender = "female";
+                    break;
+        }
+    }
+
+    public void onCheckboxClicked(View view) {
+        // Is the view now checked?
+        boolean checked = ((CheckBox) view).isChecked();
+
+        // Check which checkbox was clicked
+        switch(view.getId()) {
+            case R.id.alcohol:
+                if (checked)
+                    addiction = "alcohol";
+            else
+                break;
+            case R.id.smoking:
+                if (checked)
+                    addiction = "smoking";
+            else
+                break;
+            // TODO: Veggie sandwich
+        }
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        marital = parent.getItemAtPosition(pos).toString();
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }
+
+
+    public void onSubmitClicked(View view) {
+        String message = "";
+        message += n1.getText().toString() + " " + n4.getText().toString() + " " + gender + " " + marital + " "
+                + n5.getText().toString() + " " + addiction;
+
+
+    }
+
+    public void onResetClicked(View view) {
+
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
 }
+
+
+
