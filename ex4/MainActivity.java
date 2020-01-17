16238435778e4421d858c5db8ffe0436f76babd2
Srinivasa Arun Yeragudipati <38272686+arun-y99@users.noreply.github.com>
2020-01-17 11:17:04 +0530

    Create MainActivity.java

---
+++ ex4/MainActivity.java
@@ -0,0 +1,73 @@
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.ex4.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button bt_paint=findViewById(R.id.bt_paint);
+        Button bt_animate=findViewById(R.id.bt_animate);
+        Button bt_car=findViewById(R.id.bt_car);
+        Button bt_forward=findViewById(R.id.bt_forward);
+        Button bt_backward=findViewById(R.id.bt_backward);
+        final ImageView iv_animate=findViewById(R.id.iv_animate);
+
+        bt_animate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iv_animate.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_in));
+            }
+        });
+        bt_paint.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iv_animate.setImageResource(android.R.color.transparent);
+                Bitmap b=Bitmap.createBitmap(720,1280, Bitmap.Config.ARGB_8888);
+                iv_animate.setBackgroundDrawable(new BitmapDrawable(b));
+                Canvas canvas=new Canvas(b);
+                Paint p=new Paint();
+                p.setStrokeWidth(10);
+                p.setColor(Color.RED);
+                RectF r=new RectF(100,100,400,300);
+                canvas.drawRect(r,p);
+            }
+        });
+
+        bt_car.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iv_animate.setImageResource(R.drawable.car);
+            }
+        });
+
+        bt_forward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iv_animate.animate().translationXBy(300f).setDuration(600);
+            }
+        });
+        bt_backward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iv_animate.animate().translationXBy(-300f).setDuration(600);
+            }
+        });
+    }
+}
