c3a8d546d1ef23813c6736a44c684b22fd77730b
Srinivasa Arun Yeragudipati <38272686+arun-y99@users.noreply.github.com>
2020-01-24 12:13:51 +0530

    Update MainActivity.java

--- ex4/MainActivity.java
+++ ex4/MainActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Paint;
 import android.graphics.RectF;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
@@ -13,9 +14,10 @@ import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.androidlab.R;
+import com.example.ex4.R;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -29,6 +31,9 @@ public class MainActivity extends AppCompatActivity {
         Button bt_car=findViewById(R.id.bt_car);
         Button bt_forward=findViewById(R.id.bt_forward);
         Button bt_backward=findViewById(R.id.bt_backward);
+        Button bt_circle=findViewById(R.id.bt_circle);
+        Button bt_line=findViewById(R.id.bt_line);
+        final Bitmap b=Bitmap.createBitmap(720,1280, Bitmap.Config.ARGB_8888);;
         final ImageView iv_animate=findViewById(R.id.iv_animate);
 
         bt_animate.setOnClickListener(new View.OnClickListener() {
@@ -49,11 +54,38 @@ public class MainActivity extends AppCompatActivity {
                 iv_animate.startAnimation(AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_in));
             }
         });
+        bt_line.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onClick(View v) {
+                iv_animate.setImageResource(android.R.color.transparent);
+                iv_animate.setBackgroundDrawable(new BitmapDrawable(b));
+                Canvas canvas=new Canvas(b);
+                Paint p=new Paint();
+                p.setStrokeWidth(10);
+                p.setColor(Color.RED);
+                canvas.drawLine(100,100,600,600,p);
+            }
+        });
+        bt_circle.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+            @Override
+            public void onClick(View v) {
+                iv_animate.setImageResource(android.R.color.transparent);
+                iv_animate.setBackgroundDrawable(new BitmapDrawable(b));
+                Canvas canvas=new Canvas(b);
+                Paint p=new Paint();
+                p.setStrokeWidth(10);
+                p.setColor(Color.RED);
+                RectF r=new RectF(100,100,400,300);
+                canvas.drawArc(100,100,400,300,0,360,true,p);
+
+            }
+        });
         bt_paint.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 iv_animate.setImageResource(android.R.color.transparent);
-                Bitmap b=Bitmap.createBitmap(720,1280, Bitmap.Config.ARGB_8888);
                 iv_animate.setBackgroundDrawable(new BitmapDrawable(b));
                 Canvas canvas=new Canvas(b);
                 Paint p=new Paint();
