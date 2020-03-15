b440c8a9d085713147076fc6bc10e11a920217d6
Srinivasa Arun Yeragudipati <38272686+arun-y99@users.noreply.github.com>
2020-03-15 20:39:35 +0530

    Update MainActivity.java

--- ex7/MainActivity.java
+++ ex7/MainActivity.java
@@ -1,55 +1,58 @@
 package com.example.ex7;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.AlarmManager;
 import android.app.PendingIntent;
-import android.app.TimePickerDialog;
 import android.content.Intent;
-import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TimePicker;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
 
-    Calendar calendar;
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity
+{
+    TimePicker alarmTimePicker;
+    PendingIntent pendingIntent;
+    AlarmManager alarmManager;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        alarmTimePicker = (TimePicker) findViewById(R.id.timePicker);
+        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+    }
+    public void OnToggleClicked(View view)
+    {
+        long time;
+        if (((ToggleButton) view).isChecked())
+        {
+            Toast.makeText(MainActivity.this, "ALARM ON", Toast.LENGTH_SHORT).show();
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(Calendar.HOUR_OF_DAY, alarmTimePicker.getCurrentHour());
+            calendar.set(Calendar.MINUTE, alarmTimePicker.getCurrentMinute());
+            Intent intent = new Intent(this, AlarmReceiver.class);
+            pendingIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
 
-        final EditText et_time = findViewById(R.id.et_time);
-
-        final AlarmManager alarmManager = (AlarmManager)getSystemService(ALARM_SERVICE);
-        Intent i1 = new Intent(this,alarm.class);
-        final PendingIntent pd = PendingIntent.getBroadcast(getApplicationContext(),0,i1,0);
-
-        calendar = Calendar.getInstance();
-
-        et_time.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                new TimePickerDialog(MainActivity.this,new TimePickerDialog.OnTimeSetListener(){
-                    @Override
-                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                        et_time.setText(Integer.toString(hourOfDay)+":"+Integer.toString(minute));
-
-                        calendar.set(Calendar.HOUR_OF_DAY,hourOfDay);
-                        calendar.set(Calendar.MINUTE,minute);
-
-                        alarmManager.setExact(AlarmManager.RTC_WAKEUP,calendar.getTimeInMillis(),pd);
-
-                        Toast.makeText(MainActivity.this,"AlarmON",Toast.LENGTH_SHORT).show();
-
-                    }
-
-                },calendar.get(calendar.HOUR_OF_DAY),calendar.get(Calendar.MINUTE),false).show();
+            time=(calendar.getTimeInMillis()-(calendar.getTimeInMillis()%60000));
+            if(System.currentTimeMillis()>time)
+            {
+                if (calendar.AM_PM == 0)
+                    time = time + (1000*60*60*12);
+                else
+                    time = time + (1000*60*60*24);
             }
-        });
-
-
+            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, time, 10000, pendingIntent);
+        }
+        else
+        {
+            alarmManager.cancel(pendingIntent);
+            Toast.makeText(MainActivity.this, "ALARM OFF", Toast.LENGTH_SHORT).show();
+        }
     }
 }
